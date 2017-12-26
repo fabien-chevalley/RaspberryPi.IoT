57d96f2eee98aeabb4ca15a71c902bf28b3965aa
Fabien Chevalley <fabien.chevalley@gmail.com>
2017-12-26 11:25:08 +0100

    update configuration files to allow remote debug

--- deploy.ps1
+++ deploy.ps1
@@ -2,5 +2,5 @@
 dotnet build 
 dotnet publish -r linux-arm
 
-pscp.exe -r -pw raspberry .\RaspberryPi.IoT\bin\Debug\netcoreapp2.1\linux-arm\publish\* pi@192.168.1.66:/home/pi/jorat26/garage/
-plink.exe -v -ssh -pw raspberry pi@192.168.1.66 chmod u+x,o+x /home/pi/jorat26/garage/RaspberryPi.IoT
+pscp.exe -r -pw raspberry .\RaspberryPi.IoT\bin\Debug\netcoreapp2.1\linux-arm\publish\* pi@192.168.1.66:/home/pi/jorat26/
+plink.exe -v -ssh -pw raspberry pi@192.168.1.66 chmod u+x,o+x /home/pi/jorat26/RaspberryPi.IoT
