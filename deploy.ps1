eeb3297b3c92d7e22ddd424b53e26f2e38f9621a
Fabien Chevalley <fabien.chevalley@gmail.com>
2017-12-27 11:09:09 +0100

    correct path issue in script

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,7 @@
+Set-Location .\RaspberryPi.IoT\
 
 dotnet build 
 dotnet publish -r linux-arm
 
-pscp.exe -r -pw raspberry .\RaspberryPi.IoT\bin\Debug\netcoreapp2.1\linux-arm\publish\* pi@192.168.1.66:/home/pi/dev/
+pscp.exe -r -pw raspberry .\bin\Debug\netcoreapp2.1\linux-arm\publish\* pi@192.168.1.66:/home/pi/dev/
 plink.exe -v -ssh -pw raspberry pi@192.168.1.66 chmod u+x,o+x /home/pi/dev/RaspberryPi.IoT
